b35220a2fe3ddb8722236046fffea95553839958
Michele Tammaro <mtamma10@caledonian.ac.uk>
2013-12-18 22:18:56 +0000

    Second Commit

---
+++ MappingApplication 16.12 v2/MappingApplication/MappingApplication/AppConstants.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MappingApplication
+{
+    class AppConstants
+    {
+        //Constant values needed by multiple classes.
+        //variables are public and static to allow them to be accessed
+
+        public const int NumVenue = 13;         //total number of venues
+        public const int NumHosts = 11;         //number of hosts
+
+
+        //Coordinates of the hosts
+        public static GeoCoordinate GlasgowCoord = new GeoCoordinate(55.8580, -4.2590);                 //Glasgow Coordinates
+        public static GeoCoordinate NewDelhiCoord = new GeoCoordinate(28.633349, 77.226226);            //New Delhi Coordinates
+        public static GeoCoordinate MelbourneCoord = new GeoCoordinate(-37.814361, 144.963091);         //Melbourne Coordinates
+        public static GeoCoordinate ManchesterCoord = new GeoCoordinate(53.479329, -2.248486);          //Manchester Coordinates
+        public static GeoCoordinate KualaLumparCoord = new GeoCoordinate(3.138992, 101.686851);         //Kuala Lumpar Coordinates
+        public static GeoCoordinate VictoriaCoord = new GeoCoordinate(48.428199, -123.365739);          //Victoria Coordinates
+        public static GeoCoordinate AucklandCoord = new GeoCoordinate(-36.848487, 174.763286);          //Auckland Coordinates
+        public static GeoCoordinate EdinburghCoord = new GeoCoordinate(55.952372, -3.188764);           //Edinburgh Coordinates
+        public static GeoCoordinate BrisbaneCoord = new GeoCoordinate(-27.470925, 153.023519);          //Brisbane Coordinates
+        public static GeoCoordinate EdmontonCoord = new GeoCoordinate(53.544362, -113.490897);          //Edmonton Coordinates
+        public static GeoCoordinate ChristChurchCoord = new GeoCoordinate(-43.532006, 172.636268);      //Christ Church Coordinates
+
+        //Strings that contain the information for each host city, such as country, year and medals won
+        public static string NewDelhiInfo = "Country: India\nCity: New Delhi\nYear: 2010\nMedals: 26 (9/10/7)";                     //New Delhi games information
+        public static string MelbourneInfo = "Country: Australia\nCity: Melbourne\nYear: 2006\nMedals: 29 (11/7/11)";               //Melbourme games information
+        public static string ManchesterInfo = "Country: England\nCity: Manchester\nYear: 2002\nMedals: 30 (6/8/16)";                //Manchester games information
+        public static string KualaLumparInfo = "Country: Malaysia\nCity: Kuala Lumpar\nYear: 1998\nMedals: 12 (3/2/7)";             //Kuala Lumpar games information
+        public static string VictoriaInfo = "Country: Canada\nCity: Victoria\nYear: 1994\nMedals: 20 (6/3/11)";                     //Victoria games information
+        public static string AucklandInfo = "Country: Auckland\nCity: New Zealand\nYear: 1990\nMedals: 22 (7/7/10)";                //Auckland games information
+        public static string EdinburghInfo = "Country: Scotland\nCity: Edinburgh\nYear: 1986\nMedals: 33 (3/12/18)";                //Edinburgh games information
+        public static string BrisbaneInfo = "Country: Australia\nCity: Brisbane\nYear: 1982\nMedals: 26 (8/6/12)";                  //Brisbane games information
+        public static string EdmontonInfo = "Country: Canada\nCity: Edmonton\nYear: 1978\nMedals: 14 (3/6/5)";                      //Edmonton games information
+        public static string ChristChurchInfo = "Country: New Zealand\nCity: Christ Church\nYear: 1974\nMedals: 19 (3/5/11)";       //Christchurch games information
+
+
+
+        //Coordinates for the venues in 2014
+        public static GeoCoordinate barBud = new GeoCoordinate(56.49314, -2.74544);                         //Barry Buddons Shooting Centre Coordinates
+        public static GeoCoordinate cathBrae = new GeoCoordinate(55.794273, -4.219052);                     //Cathkin Braes Mountain Bike Trials Coordinates
+        public static GeoCoordinate celtPark = new GeoCoordinate(55.849734, -4.205625);                     //Celtic Park Coordinates
+        public static GeoCoordinate chVel = new GeoCoordinate(55.84694, -4.207384);                         //Sir Chris Hoy Velodrome Coordinates
+        public static GeoCoordinate hockCen = new GeoCoordinate(55.845831, -4.23696);                       //National Hockey Centre Coordinates
+        public static GeoCoordinate hampPark = new GeoCoordinate(55.825515, -4.25197);                      //Hampden Stadium Coordinates
+        public static GeoCoordinate ibrox = new GeoCoordinate(55.853372, -4.309069);                        //Ibrox Park Coordinates
+        public static GeoCoordinate kelvinBowls = new GeoCoordinate(55.867895, -4.288194);                  //Kelvingrove Lawn Bowls Centre Coordinates
+        public static GeoCoordinate royalPool = new GeoCoordinate(55.939923, -3.173174);                    //Royal Commonwealth Pool Coordinates
+        public static GeoCoordinate scotCampus = new GeoCoordinate(55.880583, -4.340808);                   //Scotson Sports Campus Coordinates
+        public static GeoCoordinate seccPrecinct = new GeoCoordinate(55.860923, -4.287418);                 //SECC Precinct Coordinates
+        public static GeoCoordinate strathclydePark = new GeoCoordinate(55.7971971, -4.0342997);            //Strathclyde Country Park Coordinates
+        public static GeoCoordinate tollcrossSwim = new GeoCoordinate(55.845591, -4.175798);                //Tollcross International Swimming Centre Coordinates
+
+        //strings that contain venue information, such as name, address and events
+        public static string ven0Info = "Venue: Barry Buddons Shooting Centre\nAddress: Angus & Dundee Battalion, DD7 7RY\nEvents: Shooting";                               //Barry Buddons Shooting Centre Coordinates
+        public static string ven1Info = "Venue: Cathkin Braes Mountain Bike Trails\nAddress: 201 Ardencraig Road, G45 0HR\nEvents: Mountain Biking";                        //Cathkin Braes Mountain Bike Trials Coordinates
+        public static string ven2Info = "Venue: Celtic Park\nAddress: Kerrydale Street, G40 3RE\nEvents: Opening Ceremony";                                                 //Celtic Park Coordinates
+        public static string ven3Info = "Venue: Emirates Arena including the Sir Chris Hoy Velodrome\nAddress: 1000 London Road, G40 3HY\nEvents: Cycling, Badminton";      //Sir Chris Hoy Velodrome Coordinates
+        public static string ven4Info = "Venue: Glasgow National Hockey Centre\nAddress: 8 Kings Drive, G40 1HB\nEvents: Hockey";                                           //National Hockey Centre Coordinates
+        public static string ven5Info = "Venue: Hampden Park\nAddress: Letherby Drive, G42 9BA\nEvents: Athletics";                                                         //Hampden Stadium Coordinates
+        public static string ven6Info = "Venue: Ibrox Stadium\nAddress: 150 Edmiston Drive, G51 2XD\nEvents: Rugby Sevens";                                                 //Ibrox Park Coordinates
+        public static string ven7Info = "Venue: Kelvingrove Lawn Bowls Centre\nAddress: Kelvin Way, G3 7TA\nEvents: Lawn Bowls";                                            //Kelvingrove Lawn Bowls Centre Coordinates
+        public static string ven8Info = "Venue: Royal Commonwealth Pool\nAddress: 21 Dalkeith Road, EH16 5BB\nEvents: Diving";                                              //Royal Commonwealth Pool Coordinates
+        public static string ven9Info = "Venue: Scotson Sports Campus\nAddress: 72 Danes Drive, G14 9HD\nEvents: Squash, Table Tennis";                                     //Scotson Sports Campus Coordinates
+        public static string ven10Info = "Venue: SECC Precint\nAddress: Exhibition Way, G3 8YW\nEvents: Boxing, Gymnastics, Judo, Netball, Wrestling, Weightlighting";      //SECC Precinct Coordinates
+        public static string ven11Info = "Venue: Strathclyde Country Park\nAddress: 366 Hamilton Road, ML1 3ED\nEvents: Triathlon";                                         //Strathclyde Country Park Coordinates
+        public static string ven12Info = "Venue: Tollcross International Swimming Centre\nAddress: 350 Wellshot Road, G32 7QR\nEvents: Swimming";                           //Tollcross International Swimming Centre Coordinates
+
+
+}
+}
---
+++ MappingApplication 17.12/MappingApplication/MappingApplication/Pages/hosts.xaml.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Device.Location;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using Microsoft.Phone.Maps.Controls;
+using System.Windows.Media.Imaging;
+
+namespace MappingApplication.Pages
+{
+    public partial class hosts : PhoneApplicationPage
+    {
+
+        GeoCoordinate[] hostCoord = new GeoCoordinate[AppConstants.NumHosts - 1];       //sets up an array of coordinates. Size is set to one less than total to exclude Glasgow
+        string[] hostInfo = new string[AppConstants.NumHosts - 1];                      //sets up array of strings to contain information. 
+
+
+        public hosts()
+        {
+		//constructor
+            InitializeComponent();          //method that sets up data on the view
+            this.hostMap.Center = AppConstants.GlasgowCoord;                                                                                //Coordinates of Glasgow set to the map centre
+            this.hostMap.ZoomLevel = 15; 
+            setupArrays();                  //calls method to assign constants to indexes in the array
+            insertHosts();                  //calls method to draw the venues and information on screen
+        }
+
+
+        private void setupArrays()
+        {
+            //sets up each coordinate in the array to one of the constants in App Constants
+            hostCoord[0] = AppConstants.NewDelhiCoord;
+            hostCoord[1] = AppConstants.MelbourneCoord;
+            hostCoord[2] = AppConstants.ManchesterCoord;
+            hostCoord[3] = AppConstants.KualaLumparCoord;
+            hostCoord[4] = AppConstants.VictoriaCoord;
+            hostCoord[5] = AppConstants.AucklandCoord;
+            hostCoord[6] = AppConstants.EdinburghCoord;
+            hostCoord[7] = AppConstants.BrisbaneCoord;
+            hostCoord[8] = AppConstants.EdmontonCoord;
+            hostCoord[9] = AppConstants.ChristChurchCoord;
+
+            //sets up each string in the array to one of the constants in App Constants
+            hostInfo[0] = AppConstants.NewDelhiInfo;
+            hostInfo[1] = AppConstants.MelbourneInfo;
+            hostInfo[2] = AppConstants.ManchesterInfo;
+            hostInfo[3] = AppConstants.KualaLumparInfo;
+            hostInfo[4] = AppConstants.VictoriaInfo;
+            hostInfo[5] = AppConstants.AucklandInfo;
+            hostInfo[6] = AppConstants.EdinburghInfo;
+            hostInfo[7] = AppConstants.BrisbaneInfo;
+            hostInfo[8] = AppConstants.EdmontonInfo;
+            hostInfo[9] = AppConstants.ChristChurchInfo;
+
+        }
+
+
+        private void insertHosts()
+        {
+            BitmapImage[] hosts = new BitmapImage[AppConstants.NumHosts-1];                             //creates array of images to use as overlays
+
+            for (int i = 0; i < AppConstants.NumHosts - 1; i++)                                         //loops through images
+            {
+                hosts[i] = new BitmapImage(new Uri("/Icons/Venues/hostPin.png", UriKind.Relative));     //uses the same image for each pin
+                drawHosts(hosts[i], hostCoord[i], hostInfo[i]);                                         //calls method. Passes in the host image, host coordinates
+                                                                                                        // and host information, indexed to same position int he 3 arrays
+            }
+        }
+
+        private void drawHosts(BitmapImage host, GeoCoordinate hostcoord, string hoststring)
+        {
+            MapLayer hLayer = new MapLayer();                                                           //creates new layer
+            MapOverlay hOverlay = new MapOverlay();                                                     //creates new overlay
+            Polygon hPoly = new Polygon();                                                              //creates new polygon (blank canvas)
+            hPoly.Points.Add(new Point(0, 0));                                                          //Sets first point in triangle
+            hPoly.Points.Add(new Point(38, 0));                                                         //sets second point
+            hPoly.Points.Add(new Point(19, 19));                                                        //sets third point
+            hPoly.Tap += (sender, e) => { vPoly_DoubleTap(sender, e, hoststring); };                     //calls attached event handler when the poly is clicked, passing in venstring
+
+            ImageBrush venBrush = new ImageBrush();                                                     //used to paint an area (or polygon)
+            venBrush.ImageSource = host;                                                                //sets source to passed in image
+            hPoly.Fill = venBrush;                                                                      //fills the polygon wth the brush
+            hOverlay.Content = hPoly;                                                                   //sets the content of the overlay to the filled poly
+            hOverlay.PositionOrigin = new Point(0.5, 0.5);                                              //sets origin
+            hOverlay.GeoCoordinate = hostcoord;                                                         //sets coordinates to passed coordinates
+            hLayer.Add(hOverlay);                                                                       //adds overlay to map
+            hostMap.Layers.Add(hLayer);                                                                 //adds layer to map
+        }
+
+
+        void vPoly_DoubleTap(object sender, System.Windows.Input.GestureEventArgs e, string vInfo)      //single event attached to polygons
+        {
+            MessageBox.Show(vInfo, "Host Information", MessageBoxButton.OK);                            //displays a message box with information pertaining to the games
+        }
+
+
+
+
+        private void appDiary_Click(object sender, EventArgs e)
+        {
+            //changes view to the "diary" page
+            NavigationService.Navigate(new Uri("/Pages/Diary.xaml", UriKind.Relative));
+        }
+
+        private void map_Click(object sender, EventArgs e)
+        {
+            //changes view to the "map" page
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+
+#region EventListenersForCities
+
+        private void newDelhi_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to New Delhi. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.NewDelhiCoord;           //Sets centre of map to New Delhi
+            this.hostMap.ZoomLevel = 15;                                //Zoom Level
+
+        }
+
+        private void melbourne_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to Melbourne. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.MelbourneCoord;          //Sets centre of map to Melbourne
+            this.hostMap.ZoomLevel = 15;
+        }
+
+        private void manchester_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to Manchester. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.ManchesterCoord;         //Sets centre of map to Manchester
+            this.hostMap.ZoomLevel = 15;
+        }
+
+        private void kualaLumpur_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to Kuala Lumpur. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.KualaLumparCoord;        //Sets centre of map to Kuala Lumpar
+            this.hostMap.ZoomLevel = 15;
+        }
+
+        private void victoria_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to Victoria. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.VictoriaCoord;           //Sets centre of map to Victoria
+            this.hostMap.ZoomLevel = 15;
+        }
+
+        private void auckNZ_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to Auckland. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.AucklandCoord;           //Sets centre of map to Auckland
+            this.hostMap.ZoomLevel = 15;
+        }
+
+        private void edinburgh_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to Edinburgh. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.EdinburghCoord;          //Sets centre of map to Edinburgh
+            this.hostMap.ZoomLevel = 15;
+        }
+
+        private void brisbane_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to Brisbane. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.BrisbaneCoord;           //Sets centre of map to New Delhi
+            this.hostMap.ZoomLevel = 15;
+        }
+
+        private void edmontom_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to Edmontom. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.EdmontonCoord;           //Sets centre of map to Edmonton
+            this.hostMap.ZoomLevel = 15;
+        }
+
+        private void christChurch_Click(object sender, EventArgs e)
+        {
+		//event listener that changes map view to Christ Church. Displays
+		//associated information about the games hosted by them.
+            this.hostMap.Center = AppConstants.ChristChurchCoord;       //Sets centre of map to New Delhi
+            this.hostMap.ZoomLevel = 15;
+        }
+
+#endregion
+
+    }
+}
