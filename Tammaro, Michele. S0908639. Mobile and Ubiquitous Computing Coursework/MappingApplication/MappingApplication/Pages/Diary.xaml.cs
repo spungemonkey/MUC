7e87b95df116f4e675002ff3a2ad0fc5e4b6a359
Michele Tammaro <mtamma10@caledonian.ac.uk>
2013-12-18 22:19:33 +0000

    Final Commit
    This commit contains my submitted Coursework for Mobile ad Ubiquitous
    Computing

---
+++ Tammaro, Michele. S0908639. Mobile and Ubiquitous Computing Coursework/MappingApplication/MappingApplication/Pages/Diary.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+
+namespace MappingApplication
+{
+    public partial class Diary : PhoneApplicationPage
+    {
+
+
+        //need to  instantiate an instance of IsolatedStorageSettings to store
+        //data on a WP8 app
+        IsolatedStorageSettings isoSettings = IsolatedStorageSettings.ApplicationSettings;
+
+        public Diary()
+        {
+            //constructor
+            InitializeComponent();
+            loadDiary();
+        }
+
+        private void saveDiary_Click(object sender, RoutedEventArgs e)
+        {
+            //saves the diary.
+            //Checks to see if an Isolated Storage file exists
+            //if it does not it creates a new one and inserts the
+            //inputted text into it.
+            if (!isoSettings.Contains("uDiary"))
+            {
+                isoSettings.Add("uDiary", userDiary.Text);
+            }
+            else
+            {
+                //if the file exists data is stored
+                isoSettings["uDiary"] = userDiary.Text;
+            }
+
+            //feedback shown to the user to inform them changes have been saved
+            MessageBox.Show("Diary Saved", "Saved", MessageBoxButton.OK);
+            //.Save() method called to save the data
+            isoSettings.Save();
+        }
+
+        private void deleteDiary_Click(object sender, RoutedEventArgs e)
+        {
+
+            //creates a message box that prompts the user for input
+            MessageBoxResult delIsoSto = MessageBox.Show("Are you sure you want to delete your diary?", "Warning", MessageBoxButton.OKCancel);
+
+            //if the user accepts the message box the file is deleted and
+            //the text inputted into the text box is deleted.
+            //feedback is shown
+            if (delIsoSto == MessageBoxResult.OK && isoSettings.Contains("uDiary"))
+            {
+                IsolatedStorageSettings.ApplicationSettings.Remove("uDiary");
+                userDiary.Text = "";
+                MessageBox.Show("Diary Successfully Deleted", "Deleted", MessageBoxButton.OK);
+            }
+            else
+            {
+                //Do nothing
+            }
+        }
+
+        private void loadDiary()
+        {
+            //method is run when diary view is open.
+            //if file exists the text is loaded into the user diary
+            if (isoSettings.Contains("uDiary"))
+            {
+                userDiary.Text += IsolatedStorageSettings.ApplicationSettings["uDiary"] as string;
+            }
+            else
+            {
+                userDiary.Text = "";
+            }
+        }
+
+
+        private void map_Click(object sender, EventArgs e)
+        {
+            //method to return to tge map view.
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+
+        private void prevHosts_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/hosts.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ Tammaro, Michele. S0908639. Mobile and Ubiquitous Computing Coursework/MappingApplication/MappingApplication/Pages/directions.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Device.Location;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Phone.Maps.Services;
+using Microsoft.Phone.Tasks;
+using MappingApplication.Resources;
+using Windows.Devices.Geolocation;
+using System.Windows.Media.Imaging;
+
+namespace MappingApplication.Pages
+{
+    public partial class directions : PhoneApplicationPage
+    {
+
+        GeoCoordinate[] venCoords = new GeoCoordinate[AppConstants.NumVenue];
+        string[] venInfo = new string[AppConstants.NumVenue];
+
+
+        public directions()
+        {
+            InitializeComponent();
+            getDirections();
+        }
+
+        private void getDirections()
+        {
+            MapsDirectionsTask mapDirections = new MapsDirectionsTask();
+
+            LabeledMapLocation start = new LabeledMapLocation("Label", AppConstants.ven1);
+            LabeledMapLocation end = new LabeledMapLocation("Label 2", AppConstants.ven2);
+           // mapDirections.Start = start;
+           // mapDirections.End = end;
+            mapDirections.Show();
+        }
+
+
+        private void appMap_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+
+    }
+}
